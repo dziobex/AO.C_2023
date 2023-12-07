39c818153c7a29c070e575285c1c8e6e9e366850
dziobex <58388062+dziobex@users.noreply.github.com>
2023-12-07 19:17:16 +0100

    Day 7

--- 05/02.c
+++ 05/02.c
@@ -6,8 +6,78 @@
 #define BSIZE 300
 #define ll long long
 
+typedef struct {
+    ll a, b;
+} range;
+
+int intersects( range st, range nd ) {
+    if (st.b < nd.a || nd.b < st.a)
+        return 0;
+    return 1;
+}
+
+int translate( range seeds[1000], int seed_index,  range trans, int step, int *seeds_count, range new_seeds[1000], int *new_seeds_count ) {
+    range current_seed = seeds[ seed_index ];
+
+    if ( intersects( current_seed, trans ) == 0)
+        return 0;
+
+    if ( current_seed.a >= trans.a && current_seed.b <= trans.b ) {
+        current_seed.a = step + current_seed.a - trans.a;
+        current_seed.b = step + current_seed.b - trans.a;
+
+        new_seeds[ *new_seeds_count ] = current_seed;
+        (*new_seeds_count)++;
+
+        for (int i = seed_index; i < *seeds_count - 1; ++i)
+            seeds[i] = seeds[i + 1];
+        --(*seeds_count);
+        return 1;
+    }
+
+    if ( trans.a <= current_seed.a && trans.b < current_seed.b ) {
+        range new_one;
+        new_one.a = step + current_seed.a - trans.a;
+        new_one.b = new_one.a + trans.b - current_seed.a;
+
+        current_seed.a = trans.b + 1;
+        seeds[ seed_index ] = current_seed;
+        new_seeds[ *new_seeds_count ] = new_one;
+        (*new_seeds_count)++;
+
+        return 0;
+    }
+
+    if ( current_seed.a < trans.a && current_seed.b <= trans.b ) {
+        range new_one;
+        new_one.a = step + current_seed.a - trans.a;
+        new_one.b = new_one.a + trans.b - trans.a;
+
+        current_seed.b = trans.a -1;
+        seeds[ seed_index ] = current_seed;
+        new_seeds[ *new_seeds_count ] = new_one;
+        (*new_seeds_count)++;
+
+        return 0;
+    }
+
+    new_seeds[ *new_seeds_count ] = trans;
+    (*new_seeds_count)++;
+
+    range new_one;
+    new_one.a = trans.b;
+        new_one.b = new_one.a + trans.b - trans.a;
+    current_seed.b = trans.a;
+
+    seeds[ seed_index ] = current_seed;
+    seeds[*seeds_count] = new_one;
+    (*seeds_count)++;
+
+    return 0;
+}
+
 int main( int argc, char **argv ) {
-    FILE *in = fopen( "in.txt", "r" );
+    FILE *in = fopen( "sample.txt", "r" );
 
     if ( in == NULL )
         return fputs("Nie mozna zaladowac pliku!", stderr), EXIT_FAILURE;
@@ -15,6 +85,7 @@ int main( int argc, char **argv ) {
     char buffer[BSIZE];
 
     int seeds_count = 0;
+    range seeds[1000];
 
     if (fgets(buffer, BSIZE, in) != NULL) {
         buffer[ strlen(buffer) - 1] = '\0';
@@ -23,21 +94,29 @@ int main( int argc, char **argv ) {
         tmp = strtok(buffer, " ");
 
         while (1) {
-            tmp = strtok(NULL, " ");
-            if (tmp == NULL)
+            if ( (tmp = strtok(NULL, " ")) == NULL)
                 break;
             ll start = atoll( tmp );
             tmp = strtok(NULL, " ");
             ll end = atoll( tmp );
+
+            range new_seed;
+            new_seed.a = start;
+            new_seed.b = start + end - 1;
+            seeds[ seeds_count++ ] = new_seed;
         }
 
         free( tmp );
     }
     fgets( buffer, BSIZE, in); // biore zagubionego next linea
 
-    ll min = -1;
+    // printf("%i\n", seeds_count);
 
     while ( fgets( buffer, BSIZE, in) != NULL ) {
+        
+        seeds;
+        int new_seeds_count = 0;
+        range next_gen[1000];
 
         while ( fgets( buffer, BSIZE, in) != NULL && buffer[0] != '\n' ) {
             int last_digit = strlen( buffer ) - 1;
@@ -54,15 +133,43 @@ int main( int argc, char **argv ) {
                 dig_numbers = strtok ( NULL, " " );
             }
 
-            if (min == -1)
-                min = map[0];
-            else if (map[0] != 0 && map[0] < min)
-                min = map[0];
+            range translation;
+            translation.a = map[1];
+            translation.b = map[1] + map[2] - 1;
+
+
+            // for loop, pozamieniaj
+            for ( int i = 0; i < seeds_count; ++i) {
+                // if deleted
+                // --i
+
+                if ( translate(seeds, i, translation, map[0], &seeds_count, next_gen, &new_seeds_count ) == 1)
+                    --i;
+            }
+
 
             free( dig_numbers );
         }
+
+        for (int i = 0; i < new_seeds_count; ++i) {
+            seeds[seeds_count] = next_gen[i];
+            seeds_count++;
+
+        }
+
+        for (int i = 0; i < seeds_count; ++i) {
+            
+            printf("%llu-%llu ", seeds[i].a, seeds[i].b);
+        }
+        printf("\n");
     }
 
+    ll min = seeds[0].a;
+
+    for (int i = 0; i < seeds_count; ++i)
+        if (seeds[i].a < min)
+            min = seeds[i].a;
+    
     printf("%llu\n", min);
 
     fclose( in );
---
+++ 07/02.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define B_SIZE 100
+#define H_SIZE 5 
+#define H_COUNT 1001
+#define ll long long
+
+typedef struct {
+    char hand[ H_SIZE + 1];
+    int bid;
+    int type;
+} card;
+
+card create_card( char hand[H_SIZE], int bid, int type ) {
+    card n_card = (card) { .bid = bid, .type = type };
+    strncpy(n_card.hand + 1, hand, H_SIZE);
+    n_card.hand[0] = (char)(type + '0');
+    return n_card;
+}
+
+int calculate_type( char hand[H_SIZE] ) {
+    char monitor[ 100 ] = {};
+
+    for ( int i = 0; i < H_SIZE; ++i )
+        monitor[ (int)hand[i] ]++;
+
+    
+    //251735672
+    int max = 0;
+    int min = H_SIZE;
+    int where_max = 0;
+    for ( int i = 0; i < 100; ++i) {
+        if ( i != 'J' && monitor[i] > max) {
+            
+            max = monitor[i];
+            where_max = i;
+        }
+        if ( i != 'J' &&  monitor[i] != 0 && monitor[i] < min)
+            min = monitor[ i ];
+    }
+
+    max += monitor['J'];
+    
+    if ( max >= 4)
+        return max + 2; // czyli typy 6, 7
+    if ( max == 3 && min == 2)
+        return 5;
+    if (max == 3 && min == 1)
+        return 4;
+    if (min == 1 && max == 1)
+        return 1;
+    
+    int count_pairs = 0;
+    for ( int i = 0; i < 100; ++i )
+        if ( monitor[i] == 2 )
+            ++count_pairs;
+    
+    if (count_pairs == 2)
+        return 3; // 2 pary, 1 inny
+    return 2; // 1 para, 3 inne
+}
+
+static char compare[100] = {};
+
+int cmp_cards(const void *c1, const void *c2) {
+    const card *card_1 = (const card *)c1;
+    const card *card_2 = (const card *)c2;
+
+    for (int i = 0; i <= H_SIZE; ++i) {
+        if (compare[card_1->hand[i]] > compare[card_2->hand[i]])
+            return 1;
+        else if (compare[card_1->hand[i]] < compare[card_2->hand[i]])
+            return -1;
+    }
+
+    return 0;
+}
+
+int main( int argc, char **argv ) {
+    // zainicjuj compare wartosci
+    for ( int i = 2; i <= 9; ++i )
+        compare[ i + (int)'0' ] = i;
+    compare['T'] = 10;
+    compare['J'] = 0;
+    compare['Q'] = 12;
+    compare['K'] = 13;
+    compare['A'] = 14;
+
+    FILE *in = fopen( "in.txt", "r" );
+
+    if ( in == NULL )
+        return EXIT_FAILURE;
+    
+    char buffer[B_SIZE];
+
+    card deck[ H_COUNT ];
+    int cards_count = 0;
+
+    while ( fgets( buffer, B_SIZE, in ) != NULL ) {
+        int last_index = strlen( buffer ) - 1;
+        if ( buffer[last_index] == '\n' )
+            buffer[last_index] = '\0';
+        
+        char *temp_buff;
+
+        temp_buff = strtok( buffer, " " );
+        char hand[ H_SIZE ];
+        strncpy(hand, temp_buff, H_SIZE );
+
+        temp_buff = strtok( NULL, " " );
+        int bid = atoi( temp_buff );
+
+        int type = calculate_type( hand );
+
+        deck[ cards_count ] = create_card( hand, bid, type );
+        cards_count++;
+
+        free( temp_buff );
+    }
+
+    qsort( deck, cards_count, sizeof(card), cmp_cards );
+    ll sum = 0;
+
+    for ( int i = 0; i < cards_count; ++i ) {
+        sum += (long long)(i + 1) * deck[i].bid;
+    }
+
+    printf("%llu\n", sum);
+
+    fclose( in );
+
+    return EXIT_SUCCESS;
+}
