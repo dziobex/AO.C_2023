c4289108251fb61eea13246bd2fcb6596ff1f708
dziobex <58388062+dziobex@users.noreply.github.com>
2023-12-09 09:54:16 +0100

    poprawy

--- exe-remover.sh
+++
@@ -1,1 +0,0 @@
-find . -name "*.exe" -exec rm '{}' \;
--- slave.sh
+++ slave.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+# usuwa .exe-ki i pliki input wziete ze strony https://adventofcode.com/2023
+# nastepnie automatycznie wstawia wszystkie zmiany na moje zdalne repo
+
 find . -name "*.exe" -exec rm '{}' \;
+find . -type f \( -name "in.txt" -o -name "input.txt" \) -exec rm '{}' \;
 
 if [ "$#" -ne 1 ]; then
     echo ":("
